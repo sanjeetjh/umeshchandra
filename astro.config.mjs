d37f3b13d2780d0ea4ae904f3e21a5d6886ceff8
Sanjeet Jha <sanjeetjh@gmail.com>
2026-01-03 22:07:36 +0530

    fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig(({ command }) => ({
+ export default defineConfig({
   site: 'https://sanjeetjh.github.io',
-  base: command === 'build' ? '/umeshchandra' : '/',
-  // Other Astro config options...
-}));
+  base: '/umeshchandra',
+ });
